5ff053e7155e97ed6fb29577b0843a87477daa10
himanikapadia <kapadiahimani6@gmail.com>
2026-07-22 23:57:56 +0530

    Created digi journal

--- digital_journal.py
+++ digital_journal.py
@@ -1,1 +1,16 @@
-print("Hello file")
+
+print("====== Welcome to Digi-Journal =====")
+print("Write Today's Journal")
+data=input("~Start Writting~\n")
+with open("journal.txt","a") as file:
+        file.write(data)
+print("Saved!")
+str=input("Do you want to read all journal entries? (yes/no): ")
+if str.lower() == "yes":
+        with open("journal.txt","r") as file:
+            data=file.read()
+            print(data)
+elif str.lower() == "no":
+       exit()
+else:
+       print("Invalid Choice!")
