9122e33af8404692db043f924a2fad8b9fae3c96
himanikapadia <kapadiahimani6@gmail.com>
2026-07-24 22:45:23 +0530

    Updated Changes

--- student_json_database.py
+++ student_json_database.py
@@ -22,15 +22,34 @@ while True:
             "CGPA": cgpa
         }
 
+        try:
+            # Step 1: Read existing data
+            with open("student.json","r") as file:
+                students = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):   
+            students = []  
+
+        students.append(student)
         with open("student.json","w") as file:
-            students=[]
-            students.append(student)
             json.dump(students,file,indent=4)
 
     elif choice == "2":
         with open("student.json","r") as file:
             data=json.load(file)
             print(data)
+
+    elif choice == "3":
+        name=input("Enter name to search: ")
+        with open("student.json","r") as file:
+            data=json.load(file)
+            found=0
+            for i in data:
+                if i["Name"].lower()==name.lower():
+                    print("Student Found: ",i)
+                    found=1
+                    break
+            if found == 0:
+                print("\nStudent Not Found!\n")
 
     elif choice == "4":
         print("Exited ! Student database ")
